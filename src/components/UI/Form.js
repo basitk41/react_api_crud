7dbb3b22ab0bc09cb49bb96415dec74ab9968edb
Basit Ali <basitk41@gmail.com>
2021-10-01 15:49:53 +0500

    textarea added instead of input and bug fixed

--- src/components/UI/Form.js
+++ src/components/UI/Form.js
@@ -13,16 +13,18 @@ const Form = ({ title, setTitle, body, setBody, btnText, handleSubmit }) => {
       </div>
       <div className="form-group mt-3">
         <label style={{ fontSize: "22px", fontWeight: "bold" }}>Body</label>
-        <input
+        <textarea
           value={body}
           onChange={(e) => setBody(e.target.value)}
           className="form-control form-control-lg"
           type="text"
-        />
+        ></textarea>
       </div>
       <div className="form-group mt-3" style={{ textAlign: "right" }}>
         <button
-          disabled={title.length > 0 && body.length > 0 ? false : true}
+          disabled={
+            title && title.length > 0 && body && body.length > 0 ? false : true
+          }
           className="btn btn-success"
           onClick={handleSubmit}
         >
