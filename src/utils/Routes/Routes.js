6a798a00b41a3cace0a90f3e205d8cc1db9731ce
Basit Ali <basitk41@gmail.com>
2021-10-01 11:54:14 +0500

    Routing added

--- src/utils/Routes/Routes.js
+++ src/utils/Routes/Routes.js
@@ -1,7 +1,23 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Posts from "../../components/Posts/Posts";
+import Post from "../../components/Posts/Post";
+import UpdatePost from "../../components/Posts/UpdatePost";
+import NewPost from "../../components/Posts/NewPost";
+import { Route, Redirect, Switch } from "react-router-dom";
 const Routes = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/posts" component={Posts} />
+        <Route path="/post/:id" component={Post} />
+        <Route path="/post/update/:id" component={UpdatePost} />
+        <Route path="/post/add" component={NewPost} />
+        <Redirect from="/" to="/posts" />
+      </Switch>
+    </>
+  );
 };
 
 export default Routes;
