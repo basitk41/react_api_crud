2180a698bd3dd34813227c0f1563ef7353da8d82
Basit Ali <basitk41@gmail.com>
2021-10-01 12:03:16 +0500

    nav links added.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <ul className="nav nav-pills">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href>
+        <NavLink className="nav-link" aria-current="page" to="/posts">
           Posts
-        </a>
+        </NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href>
+        <NavLink className="nav-link" to="/post/add">
           Add New Post
-        </a>
+        </NavLink>
       </li>
     </ul>
   );
