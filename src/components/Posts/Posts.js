89a6eda12925cde87d73fa6fdf7a128f6f79874d
Basit Ali <basitk41@gmail.com>
2021-10-01 13:14:38 +0500

    getting posts from api, storing at redux store and then render UI

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { mapDispatchToProps, mapStateToProps } from "../../store/helper/Posts";
-const Posts = () => {
-  return <h1>Posts</h1>;
+import List from "../UI/List";
+const Posts = ({ initPosts, posts }) => {
+  useEffect(() => {
+    initPosts();
+  }, [initPosts]);
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Posts</h1>
+      {posts.map((post) => (
+        <List key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Posts);
