494962b07eaaf6be60c187b0f1bbe7644b66ed84
Basit Ali <basitk41@gmail.com>
2021-10-01 12:50:41 +0500

    setpostid constant added

--- src/store/constants/Posts.js
+++ src/store/constants/Posts.js
@@ -1,5 +1,6 @@
 export const SETPOSTS = "SETPOSTS";
 export const SETPOST = "SETPOST";
+export const SETPOSTID = "SETPOSTID";
 export const ADDPOST = "ADDPOST";
 export const UPDATEPOST = "UPDATEPOST";
 export const DELETEPOST = "DELETEPOST";
