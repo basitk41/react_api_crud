c57d1ff69816e762e28610d0c1ddb11dda86e7f0
Basit Ali <basitk41@gmail.com>
2021-10-01 12:58:36 +0500

    bug fixed

--- src/store/reducers/Posts.js
+++ src/store/reducers/Posts.js
@@ -25,14 +25,14 @@ const Reducer = (state = initialState, action) => {
         postId: action.postId,
       };
     case Constants.ADDPOST:
-      const posts_1 = [...state];
+      const posts_1 = [...state.posts];
       posts_1.push(action.post);
       return {
         ...state,
         posts: [...posts_1],
       };
     case Constants.UPDATEPOST:
-      const posts_2 = [...state];
+      const posts_2 = [...state.posts];
       const index = posts_2.findIndex((post) => post.id === state.postId);
       if (index > -1) posts_2.splice(index, 1, action.post);
       return {
@@ -40,7 +40,7 @@ const Reducer = (state = initialState, action) => {
         posts: [...posts_2],
       };
     case Constants.DELETEPOST:
-      const posts_3 = [...state];
+      const posts_3 = [...state.posts];
       return {
         ...state,
         posts: [...posts_3.filter((post) => post.id !== action.id)],
