3a2a739da0b9cd9159f7f47acf5f18ae67d4692d
Basit Ali <basitk41@gmail.com>
2021-10-01 13:42:53 +0500

    setting up single post getting, storing amd rendering.

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,6 +1,24 @@
-import React from "react";
-const Post = () => {
-  return <h1>Single Post</h1>;
+import React, { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import Card from "../UI/Card";
+import { connect } from "react-redux";
+import { mapDispatchToProps, mapStateToProps } from "../../store/helper/Posts";
+const Post = ({ setPost, post }) => {
+  const { id } = useParams();
+  const history = useHistory();
+  useEffect(() => {
+    setPost(id);
+  }, [id, setPost]);
+  return (
+    <div>
+      <p style={{ textAlign: "right" }}>
+        <button className="btn btn-info" onClick={() => history.goBack()}>
+          Back
+        </button>
+      </p>
+      <Card post={post} />
+    </div>
+  );
 };
 
-export default Post;
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
