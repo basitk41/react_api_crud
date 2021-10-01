b3edd41e4fcad52d551afde3eb8cc37e996f37ec
Basit Ali <basitk41@gmail.com>
2021-10-01 12:09:55 +0500

    basic redux store setup

---
+++ src/store/Store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+const rootReducer = combineReducers({});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+export default store;
