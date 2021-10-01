a2d17c3c9d9eaa7be8a0923228651b100b236aa6
Basit Ali <basitk41@gmail.com>
2021-10-01 12:26:55 +0500

    loading constant added

--- src/store/constants/Posts.js
+++ src/store/constants/Posts.js
@@ -2,3 +2,4 @@ export const SETPOSTS = "SETPOSTS";
 export const ADDPOST = "ADDPOST";
 export const UPDATEPOST = "UPDATEPOST";
 export const DELETEPOST = "DELETEPOST";
+export const LOADING = "LOADING";
