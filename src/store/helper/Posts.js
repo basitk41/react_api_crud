8ed94062e57ec4600fe63291bc4f33b841de96b7
Basit Ali <basitk41@gmail.com>
2021-10-01 13:16:07 +0500

    helper setup

--- src/store/helper/Posts.js
+++ src/store/helper/Posts.js
@@ -1,6 +1,12 @@
 import * as Actions from "../actions/Posts";
 export const mapStateToProps = (state) => {
-  return {};
+  return {
+    posts: state.posts.posts,
+    userId: state.posts.userId,
+    post: state.posts.post,
+    postId: state.posts.postId,
+    loading: state.posts.loading,
+  };
 };
 export const mapDispatchToProps = (dispatch) => {
   return {
