1a5be07036418150ef51505296693b1b585c204e
Basit Ali <basitk41@gmail.com>
2021-10-01 12:54:19 +0500

    reducer added to store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-const rootReducer = combineReducers({});
+import Posts from "./reducers/Posts";
+const rootReducer = combineReducers({
+  posts: Posts,
+});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
